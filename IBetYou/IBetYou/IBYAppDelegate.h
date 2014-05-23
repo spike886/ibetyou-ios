94a8cbc114b297096aacb04ce2a79d517a6615a7
Pablo Villar <pablo@inakanetworks.com>
2014-05-23 09:29:36 -0300

    Started Xcode project.

---
+++ IBetYou/IBetYou/IBYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IBYAppDelegate.h
+//  IBetYou
+//
+//  Created by Pablo Villar on 5/23/14.
+//  Copyright (c) 2014 Inaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IBYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IBetYou/IBetYou/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IBetYou
+//
+//  Created by Pablo Villar on 5/23/14.
+//  Copyright (c) 2014 Inaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IBYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IBYAppDelegate class]));
+    }
+}
